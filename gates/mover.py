2ff8e1d1dfff0a755570739bd06027704f7e0f97
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2021-09-05 19:12:47 +0200

    Created mover.py

---
+++ gates/mover.py
@@ -0,0 +1,14 @@
+def mov(datas) :
+    
+    if self.datas1 != 0000 :
+        
+        self.datas2 = datas[1 : 4]
+        self.datas3 = self.datas1 +self.datas2
+        self.datas1 = 0000
+        self.datas2 = 0000
+        out""" number of the output """(self.datas3)
+        self.datas3 = 0000
+        
+    else :
+        
+        self.datas1 = datas[1 : 4]
