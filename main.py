853d3609e163e87db19f699ecbbd6b1cd343ac68
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2021-09-05 18:41:03 +0200

    Updated main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+def instructions() :
+    
+    
+    
+def inputs() :
+    
+    inp1 = "" """  gate input """
+    inm2 = ""
+    inp3 = ""
+    inp4 = ""
+    inp5 = ""
+    inp6 = ""
+    inp7 = ""
+    inp8 = ""
+    
+def outputs() :
+    
+    out1 = "" """  gate outputs """
+    out2 = ""
+    out3 = ""
+    out4 = ""
+    out5 = ""
+    out6 = ""
+    out7 = ""
+    out8 = ""
