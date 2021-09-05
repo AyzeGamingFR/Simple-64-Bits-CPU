c4dbf2a00161f799473bf44f5e8080fa8c09f6e0
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2021-09-05 19:21:01 +0200

    Updated mover.py

--- gates/mover.py
+++ gates/mover.py
@@ -1,13 +1,15 @@
+import ../programs/8BytesNumb.py
+
 def mov(datas) :
     
     if self.datas1 != 0000 :
         
         self.datas2 = datas[1 : 4]
         self.datas3 = self.datas1 +self.datas2
-        self.datas1 = 0000
-        self.datas2 = 0000
-        out""" number of the output """(self.datas3)
-        self.datas3 = 0000
+        output(self.datas3)
+        self.datas1 = 8BytesNumb(0000)
+        self.datas2 = 8BytesNumb(0000)
+        self.datas3 = 8BytesNumb(0000)
         
     else :
         
