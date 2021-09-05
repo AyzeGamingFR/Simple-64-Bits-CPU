f1f8a375b56f0cf056f88c88bbd7bb4b7e8b3758
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2021-09-05 19:05:19 +0200

    Updated main.py

--- main.py
+++ main.py
@@ -18,10 +18,31 @@ def instructions() :
                 
                 datasToReturn += ord(datas[i])
                 
+    def del(datas) : """ delete datas on ram instruction """
+        
+        if datas not in rams :
+            
+            
+            
+        else :
+            
+            rams.ram +datas = 000
+            
     def mov(datas) : """ move instruction """
         
+        if self.datas1 != 0000 :
         
+    def rem(datas) :
         
+        if self.datas1 != 0000 :
+            
+            self.datas2 = datas
+            self.datas = ord(self.datas1) -ord(self.datas2)
+            
+        else :
+            
+            self.datas1 = datas
+            
     def to(datas) :
         
         if len(datas) != 5 :
@@ -34,14 +55,30 @@ def instructions() :
             
 def inputs() :
     
-    inp1 = "" """ buffer gates inputs """
-    inm2 = ""
-    inp3 = ""
-    inp4 = ""
-    inp5 = ""
-    inp6 = ""
-    inp7 = ""
-    inp8 = ""
+    inp01 = "" """ buffer gates inputs """
+    inp02 = ""
+    inp03 = ""
+    inp04 = ""
+    inp05 = ""
+    inp06 = ""
+    inp07 = ""
+    inp08 = ""
+    inp09 = "" """  gates inputs """
+    inp10 = ""
+    inp11 = ""
+    inp12 = ""
+    inp13 = ""
+    inp14 = ""
+    inp15 = ""
+    inp16 = ""
+    inp17 = ""
+    inp18 = ""
+    inp19 = ""
+    inp20 = ""
+    inp21 = ""
+    inp22 = ""
+    inp23 = ""
+    inp24 = ""
     
 def outputs() :
     
