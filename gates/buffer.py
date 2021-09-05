db632a6ea19ca9582c56803b56f29c2e34aa66ff
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2021-09-05 19:17:47 +0200

    Updated buffer.py

--- gates/buffer.py
+++ gates/buffer.py
@@ -1,3 +1,11 @@
 def buf(datas) :
     
+    i = 0
     self.datas = datas[1 : 4]
+    for i < 3 :
+       
+        self.datas[i+1] += 11111111
+        
+    else :
+        
+        out""" number of the ouput """(self.datas)
